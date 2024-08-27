515cadb96eb51e2acb1402bff982c0f80173308f
samuelchoi0522 <jaedolc@gmail.com>
2024-08-27 00:54:19 -0500

    IDEFK

---
+++ backend/src/main/java/com/honey_hotel/backend/component/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.honey_hotel.backend.component;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/healthCheck")
+public class HealthCheckController {
+
+    @GetMapping
+    public String isHealthOK(){
+        return "Health is OK";
+    }
+}
