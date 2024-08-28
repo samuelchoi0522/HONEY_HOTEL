79d55de95e9886c72efac79e36aa50b9eafe13f1
samuelchoi0522 <jaedolc@gmail.com>
2024-08-27 22:16:14 -0500

    All connected now yay

---
+++ backend/src/main/java/com/honey_hotel/backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.honey_hotel.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.honey_hotel.backend.model.AppUser;
+
+@Repository
+public interface UserRepository extends JpaRepository<AppUser, Long> {
+    // You can add custom query methods if needed
+}
