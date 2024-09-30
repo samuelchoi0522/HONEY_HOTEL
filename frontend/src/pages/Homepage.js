ca6b679b0d15a8dd4f85bac2f454c8a0015a3e52
samuelchoi0522 <jaedolc@gmail.com>
2024-09-29 20:50:23 -0500

    Add yt video

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -112,6 +112,11 @@ const Homepage = () => {
                         </div>
                     </div>
                 </div>
+
+                <span className="the-story-text">{`THE STORY`}</span>
+                <div className="video-wrapper">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/NpEaa2P7qZI?si=td7dVTOVEoADBofN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
             </div>
 
 
