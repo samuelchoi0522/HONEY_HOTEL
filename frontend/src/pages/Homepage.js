61e9631b9b35650e2f52013804470994a959f9c4
samuelchoi0522 <jaedolc@gmail.com>
2024-09-29 21:29:09 -0500

    add ig posts

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -17,6 +17,14 @@ const Homepage = () => {
         }
     };
 
+    useEffect(() => {
+        //load instagrams embed.js script
+        const script = document.createElement('script');
+        script.src = "https://www.instagram.com/embed.js";
+        script.async = true;
+        document.body.appendChild(script);
+    }, []);
+
 
     return (
         <div className="homepage-container">
@@ -116,6 +124,106 @@ const Homepage = () => {
                 <span className="the-story-text">{`THE STORY`}</span>
                 <div className="video-wrapper">
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/NpEaa2P7qZI?si=td7dVTOVEoADBofN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
+
+                <span className="hashtag-honeyhotel">{`#HONEYHOTEL`}</span>
+                <div className="instagram-posts">
+                    <div className="instagram-post" dangerouslySetInnerHTML={{
+                        __html: `<blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/DAhjS0cPPQ9/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:540px; min-width:326px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);">
+                <div style="padding:16px;"> 
+                <a href="https://www.instagram.com/p/DAhjS0cPPQ9/?utm_source=ig_embed&amp;utm_campaign=loading" style=" background:#FFFFFF; line-height:0; padding:0 0; text-align:center; text-decoration:none; width:100%;" target="_blank"> 
+                <div style=" display: flex; flex-direction: row; align-items: center;"> 
+                <div style="background-color: #F4F4F4; border-radius: 50%; flex-grow: 0; height: 40px; margin-right: 14px; width: 40px;"></div> 
+                <div style="display: flex; flex-direction: column; flex-grow: 1; justify-content: center;"> 
+                <div style=" background-color: #F4F4F4; border-radius: 4px; flex-grow: 0; height: 14px; margin-bottom: 6px; width: 100px;"></div> 
+                <div style=" background-color: #F4F4F4; border-radius: 4px; flex-grow: 0; height: 14px; width: 60px;"></div>
+                </div></div>
+                <div style="padding: 19% 0;"></div> 
+                <div style="display:block; height:50px; margin:0 auto 12px; width:50px;">
+                <svg width="50px" height="50px" viewBox="0 0 60 60" version="1.1" xmlns="https://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g transform="translate(-511.000000, -20.000000)" fill="#000000">
+                <g><path d="...svg path data..."></path></g></g></g></svg>
+                </div><div style="padding-top: 8px;">
+                <div style=" color:#3897f0; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:550; line-height:18px;">View this post on Instagram</div>
+                </div><div style="padding: 12.5% 0;"></div> 
+                <div style="display: flex; flex-direction: row; margin-bottom: 14px; align-items: center;">
+                <div style=" background-color: #F4F4F4; border-radius: 50%; height: 12.5px; width: 12.5px; transform: translateX(0px) translateY(7px);"></div>
+                <div style=" background-color: #F4F4F4; height: 12.5px; transform: rotate(-45deg) translateX(3px) translateY(1px); width: 12.5px; flex-grow: 0; margin-right: 14px; margin-left: 2px;"></div> 
+                <div style=" background-color: #F4F4F4; border-radius: 50%; height: 12.5px; width: 12.5px; transform: translateX(9px) translateY(-18px);"></div></div>
+                </a></blockquote>`
+                    }}></div>
+                    <div className="instagram-post" dangerouslySetInnerHTML={{
+                        __html: `<blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/DAhjaR6vAS4/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:540px; min-width:326px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);">
+                <div style="padding:16px;"> 
+                <a href="https://www.instagram.com/p/DAhjaR6vAS4/?utm_source=ig_embed&amp;utm_campaign=loading" style=" background:#FFFFFF; line-height:0; padding:0 0; text-align:center; text-decoration:none; width:100%;" target="_blank"> 
+                <div style=" display: flex; flex-direction: row; align-items: center;"> 
+                <div style="background-color: #F4F4F4; border-radius: 50%; flex-grow: 0; height: 40px; margin-right: 14px; width: 40px;"></div> 
+                <div style="display: flex; flex-direction: column; flex-grow: 1; justify-content: center;"> 
+                <div style=" background-color: #F4F4F4; border-radius: 4px; flex-grow: 0; height: 14px; margin-bottom: 6px; width: 100px;"></div> 
+                <div style=" background-color: #F4F4F4; border-radius: 4px; flex-grow: 0; height: 14px; width: 60px;"></div>
+                </div></div>
+                <div style="padding: 19% 0;"></div> 
+                <div style="display:block; height:50px; margin:0 auto 12px; width:50px;">
+                <svg width="50px" height="50px" viewBox="0 0 60 60" version="1.1" xmlns="https://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g transform="translate(-511.000000, -20.000000)" fill="#000000">
+                <g><path d="...svg path data..."></path></g></g></g></svg>
+                </div><div style="padding-top: 8px;">
+                <div style=" color:#3897f0; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:550; line-height:18px;">View this post on Instagram</div>
+                </div><div style="padding: 12.5% 0;"></div> 
+                <div style="display: flex; flex-direction: row; margin-bottom: 14px; align-items: center;">
+                <div style=" background-color: #F4F4F4; border-radius: 50%; height: 12.5px; width: 12.5px; transform: translateX(0px) translateY(7px);"></div>
+                <div style=" background-color: #F4F4F4; height: 12.5px; transform: rotate(-45deg) translateX(3px) translateY(1px); width: 12.5px; flex-grow: 0; margin-right: 14px; margin-left: 2px;"></div> 
+                <div style=" background-color: #F4F4F4; border-radius: 50%; height: 12.5px; width: 12.5px; transform: translateX(9px) translateY(-18px);"></div></div>
+                </a></blockquote>`
+                    }}></div>
+                    <div className="instagram-post" dangerouslySetInnerHTML={{
+                        __html: `<blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/DAhjnB7MzC3/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:540px; min-width:326px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);">
+                <div style="padding:16px;"> 
+                <a href="https://www.instagram.com/p/DAhjnB7MzC3/?utm_source=ig_embed&amp;utm_campaign=loading" style=" background:#FFFFFF; line-height:0; padding:0 0; text-align:center; text-decoration:none; width:100%;" target="_blank"> 
+                <div style=" display: flex; flex-direction: row; align-items: center;"> 
+                <div style="background-color: #F4F4F4; border-radius: 50%; flex-grow: 0; height: 40px; margin-right: 14px; width: 40px;"></div> 
+                <div style="display: flex; flex-direction: column; flex-grow: 1; justify-content: center;"> 
+                <div style=" background-color: #F4F4F4; border-radius: 4px; flex-grow: 0; height: 14px; margin-bottom: 6px; width: 100px;"></div> 
+                <div style=" background-color: #F4F4F4; border-radius: 4px; flex-grow: 0; height: 14px; width: 60px;"></div>
+                </div></div>
+                <div style="padding: 19% 0;"></div> 
+                <div style="display:block; height:50px; margin:0 auto 12px; width:50px;">
+                <svg width="50px" height="50px" viewBox="0 0 60 60" version="1.1" xmlns="https://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g transform="translate(-511.000000, -20.000000)" fill="#000000">
+                <g><path d="...svg path data..."></path></g></g></g></svg>
+                </div><div style="padding-top: 8px;">
+                <div style=" color:#3897f0; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:550; line-height:18px;">View this post on Instagram</div>
+                </div><div style="padding: 12.5% 0;"></div> 
+                <div style="display: flex; flex-direction: row; margin-bottom: 14px; align-items: center;">
+                <div style=" background-color: #F4F4F4; border-radius: 50%; height: 12.5px; width: 12.5px; transform: translateX(0px) translateY(7px);"></div>
+                <div style=" background-color: #F4F4F4; height: 12.5px; transform: rotate(-45deg) translateX(3px) translateY(1px); width: 12.5px; flex-grow: 0; margin-right: 14px; margin-left: 2px;"></div> 
+                <div style=" background-color: #F4F4F4; border-radius: 50%; height: 12.5px; width: 12.5px; transform: translateX(9px) translateY(-18px);"></div></div>
+                </a></blockquote>`
+                    }}></div>
+                    <div className="instagram-post" dangerouslySetInnerHTML={{
+                        __html: `<blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/DAhjt9jMhvX/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:540px; min-width:326px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);">
+                <div style="padding:16px;"> 
+                <a href="https://www.instagram.com/p/DAhjt9jMhvX/?utm_source=ig_embed&amp;utm_campaign=loading" style=" background:#FFFFFF; line-height:0; padding:0 0; text-align:center; text-decoration:none; width:100%;" target="_blank"> 
+                <div style=" display: flex; flex-direction: row; align-items: center;"> 
+                <div style="background-color: #F4F4F4; border-radius: 50%; flex-grow: 0; height: 40px; margin-right: 14px; width: 40px;"></div> 
+                <div style="display: flex; flex-direction: column; flex-grow: 1; justify-content: center;"> 
+                <div style=" background-color: #F4F4F4; border-radius: 4px; flex-grow: 0; height: 14px; margin-bottom: 6px; width: 100px;"></div> 
+                <div style=" background-color: #F4F4F4; border-radius: 4px; flex-grow: 0; height: 14px; width: 60px;"></div>
+                </div></div>
+                <div style="padding: 19% 0;"></div> 
+                <div style="display:block; height:50px; margin:0 auto 12px; width:50px;">
+                <svg width="50px" height="50px" viewBox="0 0 60 60" version="1.1" xmlns="https://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g transform="translate(-511.000000, -20.000000)" fill="#000000">
+                <g><path d="...svg path data..."></path></g></g></g></svg>
+                </div><div style="padding-top: 8px;">
+                <div style=" color:#3897f0; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:550; line-height:18px;">View this post on Instagram</div>
+                </div><div style="padding: 12.5% 0;"></div> 
+                <div style="display: flex; flex-direction: row; margin-bottom: 14px; align-items: center;">
+                <div style=" background-color: #F4F4F4; border-radius: 50%; height: 12.5px; width: 12.5px; transform: translateX(0px) translateY(7px);"></div>
+                <div style=" background-color: #F4F4F4; height: 12.5px; transform: rotate(-45deg) translateX(3px) translateY(1px); width: 12.5px; flex-grow: 0; margin-right: 14px; margin-left: 2px;"></div> 
+                <div style=" background-color: #F4F4F4; border-radius: 50%; height: 12.5px; width: 12.5px; transform: translateX(9px) translateY(-18px);"></div></div>
+                </a></blockquote>`
+                    }}></div>
                 </div>
             </div>
 
