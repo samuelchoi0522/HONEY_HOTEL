4a3cde7891c5cff6cd748d0c40b52b8f2ed2b530
samuelchoi0522 <jaedolc@gmail.com>
2024-11-08 17:37:42 -0600

    update .env location

--- backend/src/main/java/com/honey_hotel/backend/utility/DotenvLoader.java
+++ backend/src/main/java/com/honey_hotel/backend/utility/DotenvLoader.java
@@ -2,17 +2,30 @@ package com.honey_hotel.backend.utility;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
-import org.springframework.stereotype.Component;
 
-@Component
+import java.util.logging.Logger;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DotenvLoader {
 
     @PostConstruct
     public void loadEnv() {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("EMAIL_USERNAME", dotenv.get("EMAIL_USERNAME"));
-        System.setProperty("EMAIL_PASSWORD", dotenv.get("EMAIL_PASSWORD"));
+        Dotenv dotenv = Dotenv.configure().directory("/Users/sam/Desktop/SWE 1/FULLSTACK_HOTEL_APP/.env").load();
+        Logger logger = Logger.getLogger(DotenvLoader.class.getName());
+        String dbUsername = dotenv.get("DB_USERNAME");
+        String dbPassword = dotenv.get("DB_PASSWORD");
+        String emailUsername = dotenv.get("EMAIL_USERNAME");
+        String emailPassword = dotenv.get("EMAIL_PASSWORD");
+
+        if(dbUsername != null) System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        if(dbPassword != null) System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        if(emailUsername != null) System.setProperty("EMAIL_USERNAME", dotenv.get("EMAIL_USERNAME"));
+        if (emailPassword != null)
+            System.setProperty("EMAIL_PASSWORD", dotenv.get("EMAIL_PASSWORD"));
+        
+        logger.info("Username: " + dbUsername);
+
     }
 }
