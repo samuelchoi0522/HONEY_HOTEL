121592318d8f2adc4a109f5055e4085ff50401da
samuelchoi0522 <jaedolc@gmail.com>
2024-12-07 18:51:21 -0600

    commit for merge

--- backend/src/main/java/com/honey_hotel/backend/controller/ActivitiesController.java
+++ backend/src/main/java/com/honey_hotel/backend/controller/ActivitiesController.java
@@ -1,15 +1,25 @@
 package com.honey_hotel.backend.controller;
 
-import com.honey_hotel.backend.model.Activities;
-import com.honey_hotel.backend.service.ActivitiesService;
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.util.List;
+import com.honey_hotel.backend.DTO.ActivityReservationDTO;
+import com.honey_hotel.backend.model.Activities;
+import com.honey_hotel.backend.service.ActivitiesService;
+import com.honey_hotel.backend.service.ActivityReservationService;
 
 @RestController
 @RequestMapping("/api/vacations")
@@ -18,6 +28,9 @@ public class ActivitiesController {
 
     @Autowired
     private ActivitiesService activitiesService;
+
+    @Autowired
+    private ActivityReservationService activitiesReservationService;
 
     // Add a new activity
     @PostMapping
@@ -37,4 +50,17 @@ public class ActivitiesController {
         List<Activities> availableVacations = activitiesService.getAvailableVacations(checkInDate, checkOutDate);
         return ResponseEntity.ok(availableVacations);
     }
+
+    @GetMapping("/{hotelReservationId}")
+    public ResponseEntity<ActivityReservationDTO> getActivityReservationByHotelReservationId(
+            @PathVariable Long hotelReservationId) {
+        ActivityReservationDTO activityReservationDTO = activitiesReservationService
+                .getActivityReservationByHotelReservationId(hotelReservationId);
+        if (activityReservationDTO != null) {
+            return ResponseEntity.ok(activityReservationDTO);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
 }
--- backend/src/main/java/com/honey_hotel/backend/model/ActivityReservation.java
+++ backend/src/main/java/com/honey_hotel/backend/model/ActivityReservation.java
@@ -1,8 +1,18 @@
 package com.honey_hotel.backend.model;
 
-import jakarta.persistence.*;
-
 import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "activityreservations")
@@ -14,6 +24,7 @@ public class ActivityReservation {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnore
     private AppUser user;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -22,6 +33,7 @@ public class ActivityReservation {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "hotel_reservation_id", nullable = false)
+    @JsonIgnore
     private Reservation reservation;
 
     @Column(name = "reservation_date", nullable = false)
