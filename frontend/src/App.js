6d8202480ece5185de210a18fdc63208532c93c2
Eugene Pak <eugene@koyoshiga.local>
2024-12-10 06:40:51 -0600

    Clerk Reservation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation, Navigate } from 'react-router-dom';
 
 import Homepage from './pages/Homepage';
 import Login from './pages/Login';
@@ -10,7 +10,6 @@ import NavbarFindHives from './components/Navbar_FindHives';
 import CheckRatesBar from './components/CheckRatesBar';
 import Footer from './components/Footer';
 import AccountPage from './pages/AccountPage'
-import Reservation from './pages/Reservation';
 import FindHive from './pages/FindHive';
 import RoomDetails from './pages/RoomDetails';
 import AddVacationPackage from './pages/AddVacationPackage';
@@ -18,8 +17,14 @@ import Checkout from './pages/Checkout';
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
 import AdminDashboard from './pages/AdminDashboard';
+import ReservationDetails from './pages/ReservationDetails';
+import UserDetails from './pages/UserDetails';
+import InvalidPage from './pages/InvalidPage';
+import Confirmation from './pages/Confirmation';
 
 import './styles/App.css';
+import Clerk from "./pages/Clerk";
+import ClerkReservationPage from "./pages/Clerk";
 
 function Layout() {
     const location = useLocation();
@@ -29,13 +34,13 @@ function Layout() {
         location.pathname.toLowerCase() !== '/register' &&
         location.pathname.toLowerCase() !== '/forgot-password' &&
         location.pathname.toLowerCase() !== '/reset-password';
-    
+
     const showEmptyNavbar =
         location.pathname.toLowerCase() === '/login' ||
         location.pathname.toLowerCase() === '/register' ||
         location.pathname.toLowerCase() === '/forgot-password' ||
         location.pathname.toLowerCase() === '/reset-password';
-    
+
     const showNavbarForFindHive = location.pathname.toLowerCase() === '/find-hive';
     return (
         <div className="app-container">
@@ -50,12 +55,18 @@ function Layout() {
                 <Route path="/forgot-password" element={<ForgotPassword />} />
                 <Route path="/account" element={<AccountPage />} />
                 <Route path="/find-hive" element={<FindHive />} />
-                <Route path="/reservation" element={<Reservation />} />
                 <Route path="/room-details" element={<RoomDetails />} />
                 <Route path="/add-vacation-package" element={<AddVacationPackage />} />
                 <Route path="/checkout" element={<Checkout />} />
                 <Route path="/reset-password" element={<ResetPassword />} />
                 <Route path="/admin-dashboard" element={<AdminDashboard />} />
+                <Route path="/admin-dashboard/view/:id/:bookingId" element={<ReservationDetails />} />
+                <Route path="/admin-dashboard/view/user/:id" element={<UserDetails />} />
+                <Route path="/invalid-page" element={<InvalidPage />} />
+                <Route path="/confirmation" element={<Confirmation />} />
+                <Route path="/clerk/dashboard" element={<ClerkReservationPage />} />
+
+                <Route path="*" element={<Navigate to="/invalid-page" />} />
             </Routes>
             {showNavbarAndFooter && <Footer />}
         </div>
